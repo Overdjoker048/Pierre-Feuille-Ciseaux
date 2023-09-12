b479de299c9b46d844b6365ce5fe9c6023998934
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2023-09-12 20:18:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,64 +11,65 @@ class Item():
 colorama.init()
 print(colorama.Fore.GREEN)
 
-if platform.system() == "Windows":
-    os.system("title Rock Paper Scissors")
-elif platform.system() == "Linux":
-    os.system("sudo apt-get install xtitle")
-    os.system("xtitle 'Rock Paper Scissors'")
-elif platform.system() == "Darwin":
-    os.system("touch 'Rock Paper Scissors'")
+match platform.system():
+    case "Windows": 
+        os.system("title Rock Paper Scissors")
+        clear_cmd = "cls"
+    case "Linux":
+        os.system("sudo apt-get install xtitle")
+        os.system("xtitle 'Rock Paper Scissors'")
+        clear_cmd = "reset"
+    case "Darwin":
+        os.system("touch 'Rock Paper Scissors'")
+        clear_cmd = "clear"
 
 items = [Item(name="rock", force="scissors"), Item(name="paper", force="rock"), Item(name="scissors", force="paper")]
 win = 0
 loose = 0
 
-while True:
-    gui = f""" _____            _      _____                        _____       _                          
-| __  | ___  ___ | |_   |  _  | ___  ___  ___  ___   |   __| ___ |_| ___  ___  ___  ___  ___ 
-|    -|| . ||  _|| '_|  |   __|| .'|| . || -_||  _|  |__   ||  _|| ||_ -||_ -|| . ||  _||_ -|
-|__|__||___||___||_,_|  |__|   |__,||  _||___||_|    |_____||___||_||___||___||___||_|  |___|
-                                    |_|                                                      
-Victory: {win} Defeat: {loose}
+try:
+    while True:
+        os.system(clear_cmd)
+        print(f""" _____            _      _____                        _____       _                          
+    | __  | ___  ___ | |_   |  _  | ___  ___  ___  ___   |   __| ___ |_| ___  ___  ___  ___  ___ 
+    |    -|| . ||  _|| '_|  |   __|| .'|| . || -_||  _|  |__   ||  _|| ||_ -||_ -|| . ||  _||_ -|
+    |__|__||___||___||_,_|  |__|   |__,||  _||___||_|    |_____||___||_||___||___||___||_|  |___|
+                                        |_|                                                      
+    Victory: {win} Defeat: {loose}
 
-[*] Rock
-[*] Paper
-[*] Scissors
-"""
-    if platform.system() == "Windows":
-        os.system("cls")
-    elif platform.system() == "Linux":
-        os.system("reset")
-    elif platform.system() == "Darwin":
-        os.system("clear")
-    print(gui)
-    choice = input("[Game] What do you want to play:\n>>>").lower().replace(" ", "")
-    check = False
-    for item in items:
-        if choice == item.name:
-            check = True
-            break
+    [*] Rock
+    [*] Paper
+    [*] Scissors
+    """)
+        choice = input("[Game] What do you want to play:\n>>>").lower().replace(" ", "")
+        check = False
+        for item in items:
+            if choice == item.name:
+                check = True
+                break
 
-    if check:
-        print(f"\n[You] {choice.title()}")
-        choice_pourcent = random.randint(0, 2)
-        if choice_pourcent == 0:
-            for item in items:
-                if choice == item.force:
-                    print(f"[Computer] {item.name.title()}")
-                    print("[Game] You loose")
-                    loose += 1
-        elif choice_pourcent == 1:
-            print(f"[Computer] {choice.title()}")
-            print("[Game] You equalized")
+        if check:
+            print(f"\n[You] {choice.title()}")
+            match random.randint(0, 2):
+                case 0:
+                    for item in items:
+                        if choice == item.force:
+                            print(f"[Computer] {item.name.title()}")
+                            print("[Game] You loose")
+                            loose += 1
+                case 1:
+                    print(f"[Computer] {choice.title()}")
+                    print("[Game] You equalized")
+                
+                case 2:
+                    for item in items:
+                        if choice == item.name:
+                            print(f"[Computer] {item.force.title()}")
+                            print("[Game] You win !")
+                            win += 1
+        else:
+            print("\n[Error] What you entered is not part of the expected answers.")
         
-        elif choice_pourcent == 2:
-            for item in items:
-                if choice == item.name:
-                    print(f"[Computer] {item.force.title()}")
-                    print("[Game] You win !")
-                    win += 1
-    else:
-        print("\n[Error] What you entered is not part of the expected answers.")
-    
-    input("\n[Game] Press Enter to restart...")
+        input("\n[Game] Press Enter to restart...")
+except KeyboardInterrupt:
+    print("\nGood Bye :)")
