d23463f0ffdc1cf48db7f504e0129473566791f7
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2024-09-19 09:54:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
 import random
-import os
-import colorama
 import platform
-
-class Item():
-    def __init__(self, name: str, force: str) -> None:
-        self.name = name
-        self.force = force
-
-colorama.init()
-print(colorama.Fore.GREEN)
+import os
 
 match platform.system():
-    case "Windows": 
-        os.system("title Rock Paper Scissors")
+    case "Windows":
         clear_cmd = "cls"
-    case "Linux":
-        os.system("sudo apt-get install xtitle")
-        os.system("xtitle 'Rock Paper Scissors'")
-        clear_cmd = "clear"
-    case "Darwin":
-        os.system("touch 'Rock Paper Scissors'")
+    case _:
         clear_cmd = "clear"
 
-items = [Item(name="rock", force="scissors"), Item(name="paper", force="rock"), Item(name="scissors", force="paper")]
+items = {
+    "Rock": ["Paper", "Scissors"],
+    "Paper": ["Scissors", "Rock"],
+    "Scissors": ["Rock", "Paper"]
+}
 win = 0
 loose = 0
 
 try:
     while True:
         os.system(clear_cmd)
-        print(f"""     _____            _      _____                        _____       _                          
-    | __  | ___  ___ | |_   |  _  | ___  ___  ___  ___   |   __| ___ |_| ___  ___  ___  ___  ___ 
-    |    -|| . ||  _|| '_|  |   __|| .'|| . || -_||  _|  |__   ||  _|| ||_ -||_ -|| . ||  _||_ -|
-    |__|__||___||___||_,_|  |__|   |__,||  _||___||_|    |_____||___||_||___||___||___||_|  |___|
-                                        |_|                                                      
+        print(f""" _____            _      _____                        _____       _
+| __  | ___  ___ | |_   |  _  | ___  ___  ___  ___   |   __| ___ |_| ___  ___  ___  ___  ___ 
+|    -|| . ||  _|| '_|  |   __|| .'|| . || -_||  _|  |__   ||  _|| ||_ -||_ -|| . ||  _||_ -|
+|__|__||___||___||_,_|  |__|   |__,||  _||___||_|    |_____||___||_||___||___||___||_|  |___|
+                                        |_|
     Victory: {win} Defeat: {loose}
 
     [*] Rock
     [*] Paper
     [*] Scissors
     """)
-        choice = input("[Game] What do you want to play:\n>>>").lower().replace(" ", "")
+        choice = input("[Game] What do you want to play:\n>>>").replace(" ", "").title()
         check = False
         for item in items:
             if choice == item.name:
@@ -49,29 +38,27 @@ try:
                 break
 
         if check:
-            print(f"\n[You] {choice.title()}")
-            match random.randint(0, 2):
+            print(f"\n[You] {choice}")
+            choice_pourcent = random.randint(0, 2)
+            match choice_pourcent:
                 case 0:
                     for item in items:
                         if choice == item.force:
-                            print(f"[Computer] {item.name.title()}")
+                            print(f"[Computer] {item.name}")
                             print("[Game] You loose")
                             loose += 1
-                            break
                 case 1:
-                    print(f"[Computer] {choice.title()}")
+                    print(f"[Computer] {choice}")
                     print("[Game] You equalized")
-                
                 case 2:
                     for item in items:
                         if choice == item.name:
-                            print(f"[Computer] {item.force.title()}")
+                            print(f"[Computer] {item.force}")
                             print("[Game] You win !")
                             win += 1
-                            break
         else:
             print("\n[Error] What you entered is not part of the expected answers.")
-        
+
         input("\n[Game] Press Enter to restart...")
 except KeyboardInterrupt:
-    print("\nGood Bye :)")
+    print("\n\nGood Bye :)")
